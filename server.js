6b89e8536ec2719f8e825a6b864cb363a52c556d
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2024-06-02 11:07:34 +0530

    notification done

--- server.js
+++ server.js
@@ -2,25 +2,21 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const { Server } = require('socket.io');
-const ACTIONS = require('./Actions');
+const ACTIONS = require('./src/Actions');
 const server = http.createServer(app)
 const io = new Server(server);
 var cors = require('cors')
 
-
 const compiler = require("compilex");
-const { log } = require('console');
 const options = { stats: true };
 compiler.init(options);
 
 app.use(express.json());
 app.use(cors())
 
-// map all the joined user's socket id with username in this userSocketMap object
 const userSocketMap = {}
 
 function getAllConnectedClients(roomId) {
-    // return all list of clients connected in a room (in array format so use array.from to convert it to array)
     return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
         return {
             socketId,
@@ -30,18 +26,11 @@ function getAllConnectedClients(roomId) {
 }
 
 io.on('connection', (socket) => {
-    // console.log('socket connected', socket.id);
-
-    // get the parameter sent from EditorPage.js
     socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
         userSocketMap[socket.id] = username;
-        // if first time creating room 
         socket.join(roomId);
-        // if already some user are availabe in the room then get list of clients connected in the room
         const clients = getAllConnectedClients(roomId);
-        // console.log(clients); //log in terminal -> all the clients connected in a room 
 
-        // send this information to frontend(EditorPage.js) to update the ui of editor page
         clients.forEach(({ socketId }) => {
             io.to(socketId).emit(ACTIONS.JOINED, {
                 clients,
@@ -49,53 +38,44 @@ io.on('connection', (socket) => {
                 socketId: socket.id
             })
         })
-    })
+    });
 
-
-
-    // for code change on all rooms 
     socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
-        socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });//sending to client the code received on server and used socket.in because we have to brodcast all the code to other users not to myself that's why io.to was working incorrectly
+        socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
     });
 
+    socket.on(ACTIONS.CURSOR_CHANGE, ({ roomId, cursor }) => {
+        socket.in(roomId).emit(ACTIONS.CURSOR_CHANGE, {
+            socketId: socket.id,
+            cursor,
+        });
+    });
 
-    // for code sync on all rooms 
     socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
-        io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });//sending code to only client who is just joined the room because the user wants the previous written code that's why doing this
+        io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
     });
 
-
-
-    // for disonnection
     socket.on('disconnecting', () => {
-        // get all socket rooms
-        const rooms = [...socket.rooms]; //you can you Array.from also to convert it to an array or also this syntax is correct (spread method)
+        const rooms = [...socket.rooms];
         rooms.forEach((roomId) => {
             socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
                 socketId: socket.id,
                 username: userSocketMap[socket.id],
             })
         })
-        // delete from map
         delete userSocketMap[socket.id];
-        socket.leave(); //leave the room
+        socket.leave();
     })
-})
-
-
-
-
+});
 
 app.get("/test" , (req,res) => {
     res.json("success");
 })
-// compiler.flush(function(){
-//     console.log("deleted");
-// })
+
 app.post("/runcode", function (req, res) {
     var code = req.body.code;
     var input = req.body.Input;
-    var lang = req.body.lang; // Adjusted to match the expected field name in editor.j
+    var lang = req.body.lang;
     console.log(lang);
     try {
         if (lang == "java") {
@@ -106,7 +86,7 @@ app.post("/runcode", function (req, res) {
                 compiler.compileJava(envData, code, handleResponse);
             }
         } else if (lang == "python") {
-            var envData = { OS: "windows" }; // Modify environment data as needed
+            var envData = { OS: "windows" };
             if (input) {
                 compiler.compilePythonWithInput(envData, code, input, handleResponse);
             } else {
@@ -136,18 +116,7 @@ app.post("/runcode", function (req, res) {
             res.status(400).send({ output: "Error: Execution failed" });
         }
     }
-
-    // res.status(202).json({
-    //     output : "success",
-    // });
-    
-
 });
-
-
-
-
-
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
